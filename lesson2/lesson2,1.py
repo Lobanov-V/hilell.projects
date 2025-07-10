ab9a189660fae5e8831607a4a30fe3617cc6bcce
Volodymyr Lobanov <lobanovolodymyr@icloud.com>
2025-07-10 17:37:20 +0700

    new folder and new dile

---
+++ lesson2/lesson2,1.py
@@ -0,0 +1,8 @@
+number = input("Enter a 4-digit number")
+
+n=int(number)
+
+print(n // 1000)
+print((n //100) % 10)
+print((n //10) % 10)
+print(n % 10)
---
+++ lesson2/lesson2,2.py
@@ -0,0 +1,11 @@
+number = input("Enter a 5-digit number")
+
+n=int(number)
+
+# print(n // 1000)
+# print((n //100) % 10)
+# print((n //10) % 10)
+# print(n % 10)
+#
+# print ((n // 1000) , ((n //100) % 10) , ((n //10) % 10) , (n % 10))
+print ((n % 10) , ((n //10) % 10) , ((n //100) % 10) , ((n //1000) % 10) , (n // 10000) )
