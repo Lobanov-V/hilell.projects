65d3cc1fe2b255eb415313948842ed5f64117087
Volodymyr Lobanov <lobanovolodymyr@icloud.com>
2025-07-21 12:51:14 +0700

    5.3

---
+++ lesson5/lesson5,3.py
@@ -0,0 +1,14 @@
+import string
+
+text_for_hashtag = input("Enter your text: ")
+
+words = text_for_hashtag.split()
+words_capitalized = [word.capitalize().strip(string.punctuation) for word in words]
+
+
+hashtag = '#' + ''.join(words_capitalized)
+
+if len(hashtag) > 140:
+    hashtag = hashtag[:140]
+
+print(hashtag)
