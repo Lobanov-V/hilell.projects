69e7d09c7887cbe190903bcb30122a835c10f7f9
Volodymyr Lobanov <lobanovolodymyr@icloud.com>
2025-07-24 21:37:43 +0700

    6,1 finish

---
+++ lesson6/lesson6,3.py
@@ -0,0 +1,13 @@
+import string
+
+user_letters=input("Enter two letter")
+
+first , second =user_letters.split("-")
+
+all_letters = string.ascii_letters
+
+first_index = all_letters.index(first)
+second_index = all_letters.index(second)
+
+result= all_letters[first_index:second_index +1]
+print(result)
