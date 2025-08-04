434ac8b6e9a0ff8fe19cfce52a4bf911bf5a4ced
Volodymyr Lobanov <lobanovolodymyr@icloud.com>
2025-08-04 12:11:35 +0700

    5.3 changed

--- lesson5/lesson5,3.py
+++ lesson5/lesson5,3.py
@@ -3,12 +3,38 @@ import string
 text_for_hashtag = input("Enter your text: ")
 
 words = text_for_hashtag.split()
-words_capitalized = [word.capitalize().strip(string.punctuation) for word in words]
 
+purification = []
+for word in words:
+    cleaned = ''.join(char for char in word if char not in string.punctuation)
+    purification.append(cleaned.capitalize())
 
-hashtag = '#' + ''.join(words_capitalized)
+hashtag = '#' + ''.join(purification)
+
 
 if len(hashtag) > 140:
     hashtag = hashtag[:140]
 
 print(hashtag)
+
+
+
+
+# import string
+#
+# text_for_hashtag = input("Enter your text: ")
+#
+# words = text_for_hashtag.split()
+# words_capitalized = [word.capitalize().strip(string.punctuation) for word in words]
+#
+#
+# hashtag = '#' + ''.join(words_capitalized)
+#
+# if len(hashtag) > 140:
+#     hashtag = hashtag[:140]
+#
+# print(hashtag)
+
+
+
+
