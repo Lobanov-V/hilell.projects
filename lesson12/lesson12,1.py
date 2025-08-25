cd87dd38ca151cd6366da0396bc4cea76f9d08e5
Volodymyr Lobanov <lobanovolodymyr@icloud.com>
2025-08-25 13:27:32 +0700

    12,1 chenged

--- lesson12/lesson12,1.py
+++ lesson12/lesson12,1.py
@@ -1,110 +1,71 @@
-class Product:
-    def __init__(self, name, price, description, size_category):
+class Item:
+    def __init__(self,name: str,price: int,description: str,dimensions: str)->None:
         self.name = name
         self.price = price
         self.description = description
-        self.size_category = size_category
+        self.dimensions = dimensions
 
-    def show_info(self):
-        print(f"Назва: {self.name}")
-        print(f"Ціна: {self.price} грн")
-        print(f"Опис: {self.description}")
-        print(f"Розмір: {self.size_category.capitalize()}")
-        print("〜" * 30)
+    def __str__(self)->str:
+        return f"{self.name}, price: {self.price}"
 
 
-class Customer:
-    def __init__(self, first_name, last_name, number, address=None, email=None):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.number = number
-        self.address = address
-        self.email = email
+class User:
+    def __init__(self, name: str, surname: str, numberphone: str)->None:
+        self.name = name
+        self.surname = surname
+        self.numberphone = numberphone
 
-    def show_info(self):
-        print(f"Customer: {self.first_name} {self.last_name}")
-        print(f"Number: {self.number}")
-        if self.address:
-            print(f"Addres: {self.address}")
-        if self.email:
-            print(f"Email: {self.email}")
-        print("〜" * 30)
+    def __str__(self)->str:
+        return f"{self.name} {self.surname}"
 
 
-class Order:
-    def __init__(self, customer):
-        self.customer = customer
-        self.items = {}
+class Purchase:
+    def __init__(self,user:User)->None:
+        self.products: dict[Item, int] = {}
+        self.user: User = user
+        self.total: int = 0
 
-    def add_product(self, product, quantity=1):
-        if product in self.items:
-            self.items[product] += quantity
-        else:
-            self.items[product] = quantity
+    def add_item(self, item: Item, cnt: int)->None:
+        self.products[item] = cnt
 
-    def total_price(self):
-        return sum(product.price * qty for product, qty in self.items.items())
+    def __str__(self) -> str:
+        lines: list[str] = [f"User: {self.user}", "Items:"]
+        for item, cnt in self.products.items():
+            lines.append(f"{item.name}: {cnt} pcs.")
+        return "\n".join(lines)
 
-    def __str__(self):
-        result = [
-            f"Customer: {self.customer.first_name} {self.customer.last_name}",
-            "〜" * 30,
-            "Замовлення для " + self.customer.first_name + " " + self.customer.last_name,
-            "Товари:"
-        ]
-
-        for product, qty in self.items.items():
-            total = product.price * qty
-            result.append(f"\n- {product.name} ({qty} шт.) по {product.price} грн = {total} грн")
-
-        result.append("〜" * 30)
-        result.append(f"Загальна сума: {self.total_price()} грн")
-        return '\n'.join(result)
+    def get_total(self)->int:
+        self.total = sum (item.price * cnt for item, cnt in self.products.items())
+        return self.total
 
 
-product1 = Product(
-    name="Пилосос5000",
-    price=3499,
-    description="Потужний пилосос без мішка для збору пилу.",
-    size_category="великий"
-)
+lemon = Item('lemon', 5, "yellow", "small", )
+apple = Item('apple', 2, "red", "middle", )
+print(lemon)  # lemon, price: 5
 
-product2 = Product(
-    name="НавушникиProMaxUltra",
-    price=799,
-    description="Бездротові навушники з шумозаглушенням.",
-    size_category="маленький"
-)
+buyer = User("Ivan", "Ivanov", "02628162")
+print(buyer)  # Ivan Ivanov
 
-# product1.show_info()
-# product2.show_info()
+cart = Purchase(buyer)
+cart.add_item(lemon, 4)
+cart.add_item(apple, 20)
+print(cart)
+"""
+User: Ivan Ivanov
+Items:
+lemon: 4 pcs.
+apple: 20 pcs.
+"""
+assert isinstance(cart.user, User) is True, 'Екземпляр класу User'
+assert cart.get_total() == 60, "Всього 60"
+assert cart.get_total() == 60, 'Повинно залишатися 60!'
+cart.add_item(apple, 10)
+print(cart)
+"""
+User: Ivan Ivanov
+Items:
+lemon: 4 pcs.
+apple: 10 pcs.
+"""
 
-customer1 = Customer(
-    first_name="Levi",
-    last_name="Ackerman",
-    number="+380",
-    address="Paradis Island",
-    email="<EMAIL>"
-)
-
-# customer1.show_info()
-
-order1 = Order(customer1)
-order1.add_product(product1, quantity=1)
-order1.add_product(product2, quantity=1)
-
-
-assert isinstance(order1.customer, Customer), "Екземпляр класу Customer"
-assert order1.total_price() == 4298, "Сума має бути 4298 грн"
-
-order1.add_product(product1, quantity=2)
-assert order1.total_price() == 4298 + 2 * 3499, "Сума має враховувати 2 нових товару"
-
-
-print(order1)
-
-
-
-
-
-
+assert cart.get_total() == 40
